f40b7cdccb4acb0e3bba9738b0089639fd3c3205
Nathan Dean <nathandean2@outlook.com>
2023-10-10 22:34:41 +0000

    modified:   resources/views/layouts/main-layout.blade.php

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -1,3 +1,10 @@
 <html>
     <head>
-        
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @vite('resources/css/app.css')
+    </head>
+    <body class="bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900">
+        @yield('content');
+    </body>
+</html>
