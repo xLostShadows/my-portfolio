fdc4ac54f1dc73ac52e362068de7c04cf0b4c7af
Nathan Dean <nathandean2@outlook.com>
2023-10-11 19:52:50 +0000

    modified:   public/css/app.css 	modified:   public/css/styles.css 	deleted:    public/js/script.js 	modified:   resources/views/home.blade.php 	modified:   resources/views/layouts/main-layout.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main-layout')
 
 @section('content')
-
+<h1 class="text-lg">Nathan Dean</h1>
 @endsection
--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -1,14 +1,39 @@
+<!DOCTYPE html>
+
 <html id="html">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="/css/styles.css" rel="stylesheet">
+        <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        
     </head>
     <body class="bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900">
-        <div class="container">
-            <div class="radial-gradient"></div>
+        <div class="relative">
+            <div id="responsive" class="responsive pointer-events-none fixed inset-0 z-30 transition duration-300"></div>
+            <div class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+                @yield('content')
+            </div>
         </div>
-        <script src="/js/script.js"></script>
+            
+        <script>
+            const pos = { x : 0, y : 0 };
+
+            let a = document.getElementById("responsive");
+            let b = document.getElementById("html");
+            
+
+            const saveCursorPosition = function(x, y) {
+                pos.x = x;
+                pos.y = y;
+
+                let c = `background: radial-gradient(200px at ${x}px ${y}px, rgba(29, 78, 216, 0.15), transparent 80%);`;
+
+                document.documentElement.style.setProperty('--x', pos.x);
+                document.documentElement.style.setProperty('--y', pos.y);
+                a.setAttribute('style', c);
+            }
+            document.addEventListener('mousemove', e => { saveCursorPosition(e.clientX, e.clientY);});
+        </script>
     </body>
 </html>
